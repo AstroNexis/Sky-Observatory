1f44cdf70adc6718d1496885786882b879be3ed9
pan2512811 <pan2512811@gmail.com>
2026-08-13 14:51:08 +0700

    fix: remove kotlin.android plugin since AGP 9.3.1 has built-in Kotlin support

--- sky-observatory/build.gradle.kts
+++ sky-observatory/build.gradle.kts
@@ -1,6 +1,5 @@
 plugins {
     alias(libs.plugins.android.application)
-    alias(libs.plugins.kotlin.android)
 }
 
 android {
@@ -25,6 +24,10 @@ android {
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
     }
 }
 
